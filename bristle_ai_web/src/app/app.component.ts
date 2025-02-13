cfe89d74470a7bd7a50a321f3e9fa06723e3c493
BRISTLE TECH <141940465+BristleTech@users.noreply.github.com>
2025-02-13 13:11:14 +0000

    app_component_update

--- bristle_ai_web/src/app/app.component.ts
+++ bristle_ai_web/src/app/app.component.ts
@@ -4,6 +4,8 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { HowWeWorkComponent } from './how-we-work/how-we-work.component';
 import { PartnersComponent } from './partners/partners.component';
 import { ServicesComponent } from './services/services.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +17,16 @@ import { ServicesComponent } from './services/services.component';
     HowWeWorkComponent,
     PartnersComponent,
     ServicesComponent,
+    CommonModule,
+    RouterModule,
   ],
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   activeComponent: string = 'home';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
--- bristle_ai_web/src/app/app.routes.ts
+++ bristle_ai_web/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { HowWeWorkComponent } from './how-we-work/how-we-work.component';
+import { PartnersComponent } from './partners/partners.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'how-we-work', component: HowWeWorkComponent },
+  { path: 'partners', component: PartnersComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: '**', redirectTo: '' },
+];
