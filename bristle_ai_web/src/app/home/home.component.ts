ccdbc614d62f2d9f7c97bc9f778857bfbae16d76
BRISTLE TECH <141940465+BristleTech@users.noreply.github.com>
2025-02-18 12:01:08 +0000

    image slider added in home component

--- bristle_ai_web/src/app/home/home.component.ts
+++ bristle_ai_web/src/app/home/home.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  images: string[] = [
+    'assets/images/img_1.jpg',
+    'assets/images/img_2.jpg',
+    'assets/images/img_3.jpg',
+    'assets/images/img_4.jpg',
+    'assets/images/img_5.jpg',
+  ];
 
+  currentIndex = 0;
+
+  constructor() {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 2000);
+  }
 }
