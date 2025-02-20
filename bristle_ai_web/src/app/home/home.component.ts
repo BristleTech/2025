b34c8f3a2a35ee6e815c50f09f0c1a18f85f14fe
BRISTLE TECH <141940465+BristleTech@users.noreply.github.com>
2025-02-20 07:11:55 +0000

    angular_material installed

--- bristle_ai_web/src/app/home/home.component.ts
+++ bristle_ai_web/src/app/home/home.component.ts
@@ -1,27 +1,61 @@
-import { Component } from '@angular/core';
+// 
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button'; // Import MatButtonModule
+import { MatIconModule } from '@angular/material/icon'; // Import MatIconModule for icons if needed
+import { RouterModule } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule], // Add Angular Material modules
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    animations: [
+        trigger('imageSlider', [
+            state('slide-1', style({ transform: 'translateX(0%)' })),
+            state('slide-2', style({ transform: 'translateX(-100%)' })),
+            state('slide-3', style({ transform: 'translateX(-200%)' })),
+            state('slide-4', style({ transform: 'translateX(-300%)' })),
+            state('slide-5', style({ transform: 'translateX(-400%)' })),
+            transition('* <=> *', animate('20s linear')) // Changed to linear for smoother transition
+        ])
+    ]
 })
-export class HomeComponent {
-  images: string[] = [
-    'assets/images/img_1.jpg',
-    'assets/images/img_2.jpg',
-    'assets/images/img_3.jpg',
-    'assets/images/img_4.jpg',
-    'assets/images/img_5.jpg',
-  ];
+export class HomeComponent implements OnInit, OnDestroy {
 
-  currentIndex = 0;
+    imageSources: string[] = [
+        'assets/images/img_1.jpg',
+        'assets/images/img_2.jpg',
+        'assets/images/img_3.jpg',
+        'assets/images/img_4.jpg',
+        'assets/images/bristle_logo.jpg'
+    ];
+    currentSlide = 0;
+    sliderState = `slide-${this.currentSlide + 1}`;
+    intervalId: any;
 
-  constructor() {
-    setInterval(() => {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    }, 2000);
-  }
+    ngOnInit(): void {
+        this.startSlider();
+    }
+
+    ngOnDestroy(): void {
+        this.stopSlider();
+    }
+
+    startSlider(): void {
+        this.intervalId = setInterval(() => {
+            this.nextSlide();
+        }, 20000); // 20 seconds interval
+    }
+
+    stopSlider(): void {
+        clearInterval(this.intervalId);
+    }
+
+    nextSlide(): void {
+        this.currentSlide = (this.currentSlide + 1) % this.imageSources.length;
+        this.sliderState = `slide-${(this.currentSlide) + 1}`;
+    }
 }
